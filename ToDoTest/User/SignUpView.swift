8b9d3ab7de9335ebce17b55b4b67bf640ce07366
Freesia <freesia@studiofreesia.com>
2019-06-29 17:47:09 +0900

    ログイン処理実装

--- ToDoTest/User/SignUpView.swift
+++ ToDoTest/User/SignUpView.swift
@@ -13,7 +13,12 @@ struct SignUpView : View {
     private var viewModel: UserViewModel
     @State
     private var name = ""
-    
+    @State
+    private var error = ""
+//    @State
+//    private var errShown: Bool {
+//        return !error.isEmpty
+//    }
     var body: some View {
         VStack {
             Text("Sign Up")
@@ -31,13 +36,30 @@ struct SignUpView : View {
                     .padding(.horizontal, 8.0)
                     .border(Color.gray, cornerRadius: 8)
                 
-                Button(action: { } ) {
+                Button(action: {
+                    self.viewModel.signIn(self.name) { r in
+                        switch r {
+                        case let .failure(e):
+                            self.error = e.localizedDescription
+                            print(e.localizedDescription)
+                            break
+                        case .success:
+                            break
+                        }
+                    }
+                } ) {
                     Text("Sign in")
                         .foregroundColor(Color.white)
                 }
                     .frame(height: 44.0)
                     .padding(.horizontal)
                     .background(Color.green, cornerRadius: 8.0)
+                    // ログインエラーを表示したい
+//                    .presentation($errShown) {
+//                        Alert(title: Text("ログイン失敗"),
+//                              message: Text(error),
+//                              dismissButton: .default(Text("OK")))
+//                    }
             }
             .padding()
         }
